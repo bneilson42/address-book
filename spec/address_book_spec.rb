55d453a8ce9b3d46507015644e6a81bb8ee41aa7
bneilson42 <bneilson42@ymail.com>
2016-07-12 17:05:19 -0700

    Added remove feature to address book entries

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,6 +18,23 @@ require_relative '../models/address_book.rb'
      end
    end
 
+   context "#remove_entry" do
+     it "removes an entry using name, phone_number, and email address" do
+       book = AddressBook.new
+       book.add_entry("darb nosl", "555.555.5555", "darb@gmail.com")
+
+       name = "Brad Neilson"
+       phone_number = "555.555.5555"
+       email_address = "brad@gmail.com"
+       book.add_entry(name, phone_number, email_address)
+
+       expect(book.entries.size).to eq 2
+       book.remove_entry(name, phone_number, email_address)
+       expect(book.entries.size).to eq 1
+       expect(book.entries.first.name).to eq("darb nosl")
+     end
+   end
+
    context "#add_entry" do
      it "adds only one entry to the address book" do
        book = AddressBook.new
